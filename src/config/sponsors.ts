4b40126ffa2eb73a23692bb219d90363e2896739
Shelley Wei <74998587+sheyeeyee@users.noreply.github.com>
2025-12-18 17:51:09 -0800

    added sponsors, but need to fix positioning/sizing it seems

--- src/config/sponsors.ts
+++ src/config/sponsors.ts
@@ -12,30 +12,33 @@ import altair from '../assets/logo/sponsor/svg/altair.svg';
 import kissoft from '../assets/logo/sponsor/svg/kissoft.svg';
 import hexagon from '../assets/logo/sponsor/svg/hexagon.svg';
 import ntop from '../assets/logo/sponsor/svg/nTop-Logo_Light-theme.svg';
+import solidWorks from '../assets/logo/sponsor/solidworks_logo.png';
+import ansys from '../assets/logo/sponsor/svg/ansys.svg';
+import parker from '../assets/logo/sponsor/svg/parker.svg';
 
 
 // GOLD SPONSORS
 import speedMetals from '../assets/logo/sponsor/svg/spee-d-metals.svg';
 import jergens from '../assets/logo/sponsor/svg/jergens.svg';
-import bwxt from '../assets/logo/sponsor/svg/bwxt.svg';
+// import bwxt from '../assets/logo/sponsor/svg/bwxt.svg';
 import gmnBearing from '../assets/logo/sponsor/svg/gmn-bearing.svg';
 import skf from '../assets/logo/sponsor/svg/skf.svg';
 import magna from '../assets/logo/sponsor/svg/magna.svg';
-import ansys from '../assets/logo/sponsor/svg/ansys.svg';
 import skamar from '../assets/logo/sponsor/svg/skamar.svg';
 import haas from '../assets/logo/sponsor/svg/gene-haas-foundation.svg';
 import clevelandCliffs from '../assets/logo/sponsor/cleveland-cliffs-logo.png';
 import misaMetal from '../assets/logo/sponsor/misa-metal.png';
-import solidWorks from '../assets/logo/sponsor/solidworks_logo.png';
-import parker from '../assets/logo/sponsor/svg/parker.svg';
 import tylok from '../assets/logo/sponsor/svg/tylok.svg';
-
-// SILVER SPONSORS
-import gmp from '../assets/logo/sponsor/svg/gmp-friction.svg';
-import sgs from '../assets/logo/sponsor/svg/sgs.svg';
 import thinkbox from '../assets/logo/sponsor/svg/thinkbox.svg';
 import michiganScientific from '../assets/logo/sponsor/svg/michigan-scientific.svg';
 import mastercam from '../assets/logo/sponsor/svg/mastercam.svg';
+import talanProducts from '../assets/logo/sponsor/svg/talan products.svg';
+
+
+// SILVER SPONSORS
+import gates from '../assets/logo/sponsor/gates-logo.png';
+import gmp from '../assets/logo/sponsor/svg/gmp-friction.svg';
+import sgs from '../assets/logo/sponsor/svg/sgs.svg';
 import blaser from '../assets/logo/sponsor/svg/blaser-swisslube.svg';
 import automationDirect from '../assets/logo/sponsor/svg/automation-direct.svg';
 import americanFriction from '../assets/logo/sponsor/svg/american-friction-technologies.svg';
@@ -43,16 +46,20 @@ import clark from '../assets/logo/sponsor/svg/clark.svg';
 import asi from '../assets/logo/sponsor/svg/asi.svg';
 import schunk from '../assets/logo/sponsor/svg/schunk.svg';
 import summitRacing from '../assets/logo/sponsor/summit-racing-logo.png';
+import alro from '../assets/logo/sponsor/svg/alro.svg';
+import threeDconnexion from '../assets/logo/sponsor/svg/3dconnexion.svg';
+import hypermill from '../assets/logo/sponsor/svg/hypermill.svg';
+
 
 // BRONZE SPONSORS
 import nordlock from '../assets/logo/sponsor/svg/nord-lock-group.svg';
+import holley from '../assets/logo/sponsor/HolleyPB_White_RGB copy.png';
 import southington from '../assets/logo/sponsor/svg/southington.svg';
+import redBull from '../assets/logo/sponsor/svg/redbull.svg';
 import ptg from '../assets/logo/sponsor/svg/ptg.svg';
 import boltDepot from '../assets/logo/sponsor/svg/bolt-depot.svg';
 import fkRodEnds from '../assets/logo/sponsor/svg/fk-rod-ends.svg';
-import alro from '../assets/logo/sponsor/svg/alro.svg';
 import fathomRealty from '../assets/logo/sponsor/fathom-realty-logo.png';
-import gates from '../assets/logo/sponsor/gates-logo.png';
 import microMeasurements from '../assets/logo/sponsor/micro-measurements-logo.png';
 import oshCut from '../assets/logo/sponsor/oshcut-logo.png';
 import stampedeDie from '../assets/logo/sponsor/stampede-die-logo.png';
@@ -61,6 +68,15 @@ import nsk from '../assets/logo/sponsor/nsk-logo.png';
 import commercialSteelTreating from '../assets/logo/sponsor/commercial-steel-logo.png';
 import tmacMachine from '../assets/logo/sponsor/tmac-machine-logo.png';
 import ppg from '../assets/logo/sponsor/ppg-logo.png';
+import carbideDepot from '../assets/logo/sponsor/svg/carbide depot.svg';
+import titaniumJoe from '../assets/logo/sponsor/svg/titanium joe.svg';
+import sendcutsend from '../assets/logo/sponsor/svg/sendcutsend.svg';
+import bicycleFrameDepot from '../assets/logo/sponsor/svg/bicycle frame depot.svg';
+import trippWells from '../assets/logo/sponsor/svg/trippwells inc.svg';
+import motec from '../assets/logo/sponsor/svg/motec.svg';
+import extremePowderCoating from '../assets/logo/sponsor/svg/extreme powder coating.svg';
+import simutech from '../assets/logo/sponsor/svg/simutech group.svg';
+import raisingCanes from '../assets/logo/sponsor/svg/raising canes.svg';
 
 export interface Sponsor {
     name: string;
@@ -83,27 +99,29 @@ export const sponsors = {
         { name: "KISSsoft", logo: kissoft, url: "https://www.kisssoft.com/en" },
         { name: "Hexagon", logo: hexagon, url: "https://hexagon.com/" },
         { name: "nTop", logo: ntop, url: "https://www.ntop.com/" },
+        { name: "SolidWorks", logo: solidWorks, url: "https://www.solidworks.com/" },
+        { name: "ANSYS", logo: ansys, url: "https://www.ansys.com/" },
+        { name: "Parker", logo: parker, url: "https://www.parker.com/us/en/home.html" },
     ],
     gold: [
         { name: "Spee-D Metals", logo: speedMetals, url: "https://speedmetals.com/" },
         { name: "Jergens", logo: jergens, url: "https://www.jergensinc.com/" },
-        { name: "BWXT", logo: bwxt, url: "https://www.bwxt.com/" },
+        // { name: "BWXT", logo: bwxt, url: "https://www.bwxt.com/" },
         // { name: "Orange Vise", logo: orangeVise, url: "https://www.orangevise.com/" },
         { name: "GMN Bearing", logo: gmnBearing, url: "https://www.gmnbt.com/" },
         { name: "SKF", logo: skf, url: "https://www.skf.com/us" },
         { name: "Magna", logo: magna, url: "https://www.magna.com/" },
-        { name: "ANSYS", logo: ansys, url: "https://www.ansys.com/" },
         { name: "Skamar", logo: skamar, url: "https://skamar.com/" },
         { name: "Gene Haas Foundation", logo: haas, url: "https://www.ghaasfoundation.org/" },
         { name: "Cleveland Cliffs", logo: clevelandCliffs, url: "https://www.clevelandcliffs.com/" },
         { name: "Misa Metal", logo: misaMetal, url: "https://www.misametal.com/" },
-        { name: "SolidWorks", logo: solidWorks, url: "https://www.solidworks.com/" },
         { name: "Michigan Scientific", logo: michiganScientific, url: "https://www.michsci.com/" },
         { name: "Alro", logo: alro, url: "https://www.alro.com/" },
-        { name: "Parker", logo: parker, url: "https://www.parker.com/us/en/home.html" },
         { name: "Tylok", logo: tylok, url: "https://www.tylok.com/" },
+        { name: "Talan Products", logo: talanProducts, url: "https://www.talanproducts.com/" },
     ],
     silver: [
+        { name: "Gates", logo: gates, url: "https://www.gates.com/us/en.html" },
         { name: "GMP Friction", logo: gmp, url: "https://gmpfriction.com/" },
         { name: "SGS", logo: sgs, url: "https://www.sgs.com/en" },
         { name: "Sears think[box]", logo: thinkbox, url: "https://case.edu/thinkbox/" },
@@ -115,15 +133,18 @@ export const sponsors = {
         { name: "Anodizing Specialists", logo: asi, url: "https://www.anodizingspecialists.com/" },
         { name: "Schunk", logo: schunk, url: "https://schunk.com/us/en" },
         { name: "Summit Racing", logo: summitRacing, url: "https://www.summitracing.com/" },
+        { name: "3Dconnexion", logo: threeDconnexion, url: "https://3dconnexion.com/us/" },
+        { name: "HyperMill", logo: hypermill, url: "https://www.openmind-tech.com/en-us/cam/product-overview/" },
     ],
     bronze: [
         { name: "Nord-Lock Group", logo: nordlock, url: "https://www.nord-lock.com/en-us/" },
+        { name: "Holley", logo: holley, url: "https://www.holley.com/" },
         { name: "Southington Offroad", logo: southington, url: "https://southingtonoffroad.com/" },
         { name: "Performance Titanium Group", logo: ptg, url: "https://performancetitanium.com/" },
+        { name: "Red Bull", logo: redBull, url: "https://www.redbull.com/us-en" },
         { name: "Bolt Depot", logo: boltDepot, url: "https://boltdepot.com/" },
         { name: "FK Rod Ends", logo: fkRodEnds, url: "https://www.fkrodends.com/" },
         { name: "Fathom Realty", logo: fathomRealty, url: "https://fathomrealty.com/" },
-        { name: "Gates", logo: gates, url: "https://www.gates.com/us/en.html" },
         { name: "Micro-Measurements", logo: microMeasurements, url: "https://www.micro-measurements.com/" },
         { name: "OSH Cut", logo: oshCut, url: "https://www.oshcut.com/" },
         { name: "Stampede Die", logo: stampedeDie, url: "https://stampededie.com/" },
@@ -131,7 +152,16 @@ export const sponsors = {
         { name: "NSK", logo: nsk, url: "https://www.nsk.com/" },
         { name: "Commercial Steel Treating Co", logo: commercialSteelTreating, url: "https://www.commercialsteeltreating.com/" },
         { name: "T-Mac Machine Inc.", logo: tmacMachine, url: "" },
-        { name: "PPG", logo: ppg, url: "https://www.ppg.com/" }
+        { name: "PPG", logo: ppg, url: "https://www.ppg.com/" },
+        { name: "Carbide Depot", logo: carbideDepot, url: "https://www.carbidedepot.com/" },
+        { name: "Titanium Joe", logo: titaniumJoe, url: "https://www.titaniumjoe.com/" },
+        { name: "SendCutSend", logo: sendcutsend, url: "https://sendcutsend.com/" },
+        { name: "Bicycle Frame Depot", logo: bicycleFrameDepot, url: "https://bicycleframedepot.us/" },
+        { name: "TrippWells", logo: trippWells, url: "https://www.trippwells.com/" },
+        { name: "MoTeC", logo: motec, url: "https://www.milspecwiring.com/MoTeC_c_335.html" },
+        { name: "Extreme Powder Coating", logo: extremePowderCoating, url: "https://ohiopowdercoat.com/" },
+        { name: "SimuTech Group", logo: simutech, url: "https://simutechgroup.com/" },
+        { name: "Raising Canes", logo: raisingCanes, url: "https://raisingcanes.com/" },
     ],
 };
 
